14e5445e79f1876175b1debf7aa19520989b51ff
Jay Smith <jsmithb117@gmail.com>
2023-06-20 10:52:25 -0700

    Complete

--- src/App.js
+++ src/App.js
@@ -6,14 +6,36 @@ function App() {
   const [number2, setNumber2] = useState();
   const [total, setTotal] = useState(0);
 
+  function num1ChangeHandler(number) {
+    setNumber1(number)
+  }
+
+  function num2ChangeHandler(number) {
+    setNumber2(number)
+  }
+
+  function clickHandler() {
+    setTotal(number1 + number2)
+  }
+
   return (
     <div>
       <h2>Adding Two Numbers</h2>
-      <input placeholder="First Number" type="number" />
-      <input placeholder="Second Number" type="number" />
+      <input
+        placeholder="First Number"
+        type="number"
+        value={number1}
+        onChange={(e) => num1ChangeHandler(parseInt(e.target.value))}
+      />
+      <input
+        placeholder="Second Number"
+        type="number"
+        value={number2}
+        onChange={(e) => num2ChangeHandler(parseInt(e.target.value))}
+      />
 
-      <button>Add Two Numbers</button>
-      <p>Total:</p>
+      <button onClick={clickHandler}>Add Two Numbers</button>
+      <p>Total: {total}</p>
     </div>
   );
 }
