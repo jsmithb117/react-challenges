1ea3ed787278dc66735c9e361d20d14071149f1e
Jay Smith <jsmithb117@gmail.com>
2023-06-20 10:44:03 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [number1, setNumber1] = useState();
+  const [number2, setNumber2] = useState();
+  const [total, setTotal] = useState(0);
+
   return (
-    <div className="App">
-      App
+    <div>
+      <h2>Adding Two Numbers</h2>
+      <input placeholder="First Number" type="number" />
+      <input placeholder="Second Number" type="number" />
+
+      <button>Add Two Numbers</button>
+      <p>Total:</p>
     </div>
   );
 }
