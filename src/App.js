88c6464ff88742b93e5cae23bc2bd286cc929721
Jay Smith <jsmithb117@gmail.com>
2023-06-20 10:56:21 -0700

    Complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import { useState } from 'react';
 
 const App = () => {
-  const [] = useState(0);
+  const [count, setCount] = useState(0);
 
+  function incrementHandler() {
+    setCount(count + 1)
+  }
+  function decrementHandler() {
+    setCount(count - 1)
+  }
   return (
     <div>
-      <h2>Counter: 0</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h2>Counter: {count}</h2>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
     </div>
   );
 };
