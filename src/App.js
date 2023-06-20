aa3d8a5653fb28f02a9566006eb24ef75cb71f66
Jay Smith <jsmithb117@gmail.com>
2023-06-20 10:13:32 -0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [] = useState("");
+  const [text, setText] = useState("");
+  const [enabled, setEnabled] = useState(false);
+
+  function textChange(newText) {
+    setText(newText)
+  }
+
+  function clickHandler() {
+    if (enabled) {
+      console.log(`Submitting ${text}`)
+    } else {
+      console.log('disabled')
+    }
+  }
+
+  useEffect(() => {
+    if (text) {
+      setEnabled(true)
+    } else {
+      setEnabled(false)
+    }
+  }, [text])
 
   return (
     <>
       <h3>Disable Button Challenge</h3>
-      <input type="text" />
-      <button>Submit</button>
+      <input type="text" value={text} onChange={(e) => textChange(e.target.value)}/>
+      <button onClick={clickHandler}>Submit</button>
     </>
   );
 }
